b612e6895b8a618568f8bd819f57a26ba5a6672d
XIWEN HU <156972775+xycww@users.noreply.github.com>
2024-11-14 15:55:01 -0500

    Add files via upload

---
+++ starter_folder/scripts/03-test_data.R
@@ -0,0 +1,22 @@
+#### Preamble ####
+# Purpose: Sanity check of the data
+# Author: Rohan Alexander
+# Date: 19 September 2024
+# Contact: rohan.alexander@utoronto.ca
+# License: MIT
+# Pre-requisites: Need to have simulated data
+# Any other information needed? None.
+
+
+#### Workspace setup ####
+library(tidyverse)
+
+
+#### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
+
+# Test for negative numbers
+data$number_of_marriage |> min() <= 0
+
+# Test for NAs
+all(is.na(data$number_of_marriage))
